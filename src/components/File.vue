cebeec5b54d40953e27e5fd9a0fd95877eec3402
Alan Jimenez Arandia <aljiar23@gmail.com>
2023-08-14 16:12:30 -0400

    Add method to load photo

--- src/components/File.vue
+++ src/components/File.vue
@@ -1,82 +1,89 @@
 <template>
   <page size="letter">
-  <div class="component-with-shapes-and-image">
-    <!-- Círculo amarillo -->
-    <div class="yellow-circle"></div>
-
-    <div class="blue-rectangle">
-      <aside class="personalProfile">
-        <div>
-          <p>
-            {{ this.name + ' ' + this.lastNameF + ' ' + this.lastNameM }}</p>
-          <p>
-            {{ this.numberId }}</p>
-          <p>
-            {{ this.dateOfBirth }}</p>
-          <p>
-            {{ this.gender }}</p>
-          <p>{{ this.cityRadication + ', ' + this.selectedCountry }}
-          </p>
-          <p> {{ this.addres }} </p>
-          <p>
-            {{ this.personalPhone }}
-          </p>
-          <p>
-            {{ this.email }}
-          </p>
+    <div class="component-with-shapes-and-image">
+      <!-- Círculo amarillo -->
+      <div class="yellow-circle">
+        <div class="white-circle">
+          <v-avatar size="200">
+            <img :src="img" width="200" height="200">
+          </v-avatar>
         </div>
-      </aside>
-    </div>
-    <!-- Rectángulo amarillo -->
-    <div class="yellow-rectangle"></div>
+      </div>
 
-    <div class="education">
-      <div class="degrees">
-        <h1>
-          Estudios Pregrado
-        </h1>
-        <ol v-for="(degree, index) in degrees" :key="index" class="cards">
-          <p>{{ degree.universityDegree }}</p>
-          <p>{{ degree.careerDegree }}</p>
-          <p>{{ degree.levelOfDegree }}</p>
-          <p>{{ degree.countryDegree }}</p>
-          <p>{{ degree.graduationYearDegree }}</p>
-          <p>{{ degree.graduationModalityDegree }}</p>
-        </ol>
+      <div class="blue-rectangle">
+        <aside class="personalProfile">
+          <div>
+            <p>
+              {{ this.name + ' ' + this.lastNameF + ' ' + this.lastNameM }}</p>
+            <p>
+              {{ this.numberId }}</p>
+            <p>
+              {{ this.dateOfBirth }}</p>
+            <p>
+              {{ this.gender }}</p>
+            <p>{{ this.cityRadication + ', ' + this.selectedCountry }}
+            </p>
+            <p> {{ this.addres }} </p>
+            <p>
+              {{ this.personalPhone }}
+            </p>
+            <p>
+              {{ this.email }}
+            </p>
+          </div>
+        </aside>
       </div>
-      <div class="degrees">
-        <h1>
-          Estudios Postgrado
-        </h1>
-        <ol v-for="(postDegree, index) in postDegrees" :key="index" class="cards">
-          <p>{{ postDegree.universityPostDegree }}</p>
-          <p>{{ postDegree.namePostDegree }}</p>
-          <p>{{ postDegree.titlePostDegree }}</p>
-          <p>{{ postDegree.countryPostDegree }}</p>
-          <p>{{ postDegree.graduationYearPostDegree }}</p>
-          <p>{{ postDegree.graduationModalityPostDegree }}</p>
-        </ol>
-      </div>
-      <div class="degrees">
-        <h1>
-          Cursos
-        </h1>
-        <ol v-for="(course, index) in courses" :key="index" class="cards">
-          <p>{{ course.institutionCourse }}</p>
-          <p>{{ course.nameCourse }}</p>
-          <p>{{ course.countryCourse }}</p>
-          <p>{{ course.yearCourse }}</p>
-        </ol>
+      <!-- Rectángulo amarillo -->
+      <div class="yellow-rectangle"></div>
+
+      <div class="education">
+        <div class="degrees">
+          <h1>
+            Estudios Pregrado
+          </h1>
+          <ol v-for="(degree, index) in degrees" :key="index" class="cards">
+            <p>{{ degree.universityDegree }}</p>
+            <p>{{ degree.careerDegree }}</p>
+            <p>{{ degree.levelOfDegree }}</p>
+            <p>{{ degree.countryDegree }}</p>
+            <p>{{ degree.graduationYearDegree }}</p>
+            <p>{{ degree.graduationModalityDegree }}</p>
+          </ol>
+        </div>
+        <div class="degrees">
+          <h1>
+            Estudios Postgrado
+          </h1>
+          <ol v-for="(postDegree, index) in postDegrees" :key="index" class="cards">
+            <p>{{ postDegree.universityPostDegree }}</p>
+            <p>{{ postDegree.namePostDegree }}</p>
+            <p>{{ postDegree.titlePostDegree }}</p>
+            <p>{{ postDegree.countryPostDegree }}</p>
+            <p>{{ postDegree.graduationYearPostDegree }}</p>
+            <p>{{ postDegree.graduationModalityPostDegree }}</p>
+          </ol>
+        </div>
+        <div class="degrees">
+          <h1>
+            Cursos
+          </h1>
+          <ol v-for="(course, index) in courses" :key="index" class="cards">
+            <p>{{ course.institutionCourse }}</p>
+            <p>{{ course.nameCourse }}</p>
+            <p>{{ course.countryCourse }}</p>
+            <p>{{ course.yearCourse }}</p>
+          </ol>
+        </div>
       </div>
     </div>
-  </div>
-</page>
-
+  </page>
 </template>
 
 <script>
 import { database } from '../firebase/firebase'
 import { collection, doc, getDoc, getDocs } from 'firebase/firestore'
+import { getStorage, ref, getDownloadURL } from 'firebase/storage'
+
 export default {
   data() {
     return {
@@ -98,6 +105,7 @@ export default {
       degrees: [],
       postDegrees: [],
       courses: [],
+      img: null,
     }
   },
   created() {
@@ -105,6 +113,7 @@ export default {
     this.getDegrees()
     this.getPostDegrees()
     this.getCourses()
+    this.loadProfileImage()
   },
 
   methods: {
@@ -167,6 +176,18 @@ export default {
         })
       })
     },
+    loadProfileImage() {
+      const storage = getStorage();
+      getDownloadURL(ref(storage, this.idUser + '/photoProfile/AlanJimenez-PhotoProfile.png'))
+        .then((url) => {
+          // Or inserted into an <img> element
+          this.img = url
+        })
+        .catch((error) => {
+          // Handle any errors
+        });
+    }
+
   },
 };
 </script>
@@ -195,12 +216,29 @@ export default {
   /* Ajustar la posición vertical del círculo */
   left: 0.7in;
   /* Ajustar la posición horizontal del círculo */
+  width: 220px;
+  /* Diámetro del círculo */
+  height: 220px;
+  /* Diámetro del círculo */
+  border-radius: 50%;
+  background-color: #FFDF0B;
+  /* Color amarillo */
+  z-index: 1;
+  /* Asegura que el círculo esté por encima del rectángulo */
+  position: relative;
+}
+
+.white-circle {
+  top: 0.1in;
+  /* Ajustar la posición vertical del círculo */
+  left: 0.1in;
+  /* Ajustar la posición horizontal del círculo */
   width: 200px;
   /* Diámetro del círculo */
   height: 200px;
   /* Diámetro del círculo */
   border-radius: 50%;
-  background-color: #FFDF0B;
+  background-color: white;
   /* Color amarillo */
   z-index: 1;
   /* Asegura que el círculo esté por encima del rectángulo */
@@ -261,14 +299,15 @@ export default {
   color: #162D4A;
   font-weight: bold;
 }
+
 @page {
-  size: 8.5in 11in ;
+  size: 8.5in 11in;
   width: 8.5in;
   height: 11in;
   margin: 0;
 }
-.education {
-  top :0%;
-}
 
+.education {
+  top: 0%;
+}
 </style>
