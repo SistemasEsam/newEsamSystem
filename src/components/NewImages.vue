5a58af266e6b333c099d69cac705fed53cc9e92e
Irey75003319 <yeri.soruco@esam.edu.bo>
2023-09-19 09:32:44 -0400

    New home

---
+++ src/components/NewImages.vue
@@ -0,0 +1,130 @@
+<template>
+    <v-app>
+      <v-carousel cycle hide-delimiters :show-arrows="false">
+        <v-carousel-item v-for="(item, index) in carouselItems" :key="index">
+          <v-sheet
+            :style="{ 'background-image': `url(${item.backgroundImage})`, 'background-size': 'cover' }"
+            height="100%"
+            class="d-flex flex-column align-center justify-center"
+          >
+            <h2 class="custom-title">{{ item.title }}</h2>
+            <h3 class="custom-subtitle">{{ item.subtitle }}</h3>
+            <p class="custom-description">{{ item.description }}</p>
+            <v-btn class="custom-btn" color="primary">{{ item.buttonText }}</v-btn>
+          </v-sheet>
+        </v-carousel-item>
+      </v-carousel>
+      <p class="custome-more">CONOCE MAS</p>
+      <v-btn @click="scrollToComponent" variant="plain"  class="custom-button" >
+       <v-icon left>
+        mdi-chevron-down
+       </v-icon>
+
+      </v-btn>
+      <div ref="componentToScrollTo" style="height: 500px; background-color: lightgray">
+        <!-- Contenido del componente al que se desplazará -->
+        <p>Este es el componente al que te desplazarás.</p>
+      </div>
+
+
+    </v-app>
+  </template>
+  
+  <script>
+
+
+  export default {
+methods:{
+  scrollToComponent() {
+        const element = this.$refs.componentToScrollTo;
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+      }
+},
+ 
+    data() {
+      return {
+     
+        carouselItems: [
+          {
+            title: 'Imagen 1',
+            subtitle: 'Subtítulo de la Imagen 1',
+            description: 'Descripción de la Imagen 1.',
+            buttonText: 'Botón 1',
+            backgroundImage: require('@/assets/BANCA.png'), // Ruta de la imagen de fondo
+          },
+          {
+            title: 'Imagen 2',
+            subtitle: 'Subtítulo de la Imagen 2',
+            description: 'Descripción de la Imagen 2.',
+            buttonText: 'Botón 2',
+            backgroundImage: require('@/assets/GERENCIATRIBUTARIA.png'), // Ruta de la imagen de fondo
+          },
+          // Agrega más objetos para más imágenes
+        ],
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Estilos personalizados */
+  .custom-title {
+    font-size: 30px; /* Tamaño de fuente personalizado */
+    font-weight: bold-semi-regular; /* Tipo de letra en negrita */
+    color: #ff5722; /* Color de letra personalizado */
+    text-align: left; /* Alineación a la izquierda */
+    margin-left: 1000px; /* Margen izquierdo para mover el título hacia la izquierda */
+  }
+    .custom-subtitle {
+  font-size: 60px; /* Tamaño de fuente personalizado */
+  font-weight: Black; /* Tipo de letra en negrita */
+  color: #2196F3; /* Color de letra personalizado */
+  text-align: left; /* Alineación centrada */
+  margin-left: 1000px; /* Margen superior para separar el subtítulo del título */
+  }
+  .custom-description {
+  font-size: 16px; /* Tamaño de fuente personalizado */
+  font-weight: bold; /* Tipo de letra en negrita */
+  color: #ff5722; /* Color de letra personalizado */
+  text-align: left; /* Alineación a la izquierda */
+  margin-left: 1000px; /* Margen izquierdo para mover la descripción hacia la izquierda */
+}
+.custom-btn {
+  font-size: 16px; /* Tamaño de fuente personalizado */
+  font-weight: bold; /* Tipo de letra en negrita */
+  color: #ff5722; /* Color de letra personalizado */
+  text-align: left; /* Alineación a la izquierda */
+  margin-left: 1000px; /* Margen izquierdo para mover la descripción hacia la izquierda */
+}
+.v-carousel-item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.custome-more{
+
+  font-size: 30px; /* Tamaño de fuente personalizado */
+  font-weight: bold; /* Tipo de letra en negrita */
+  color: #ff5722; /* Color de letra personalizado */
+  text-align: left; /* Alineación a la izquierda */
+  margin-left: 585px; /* Margen izquierdo para mover la descripción hacia la izquierda */
+
+
+
+}
+
+.custom-button {
+ 
+   
+    color: #D8D2D2; /* Cambia el color del texto a #D8D2D2 */
+    width: 100px; /* Cambia el ancho según tus preferencias */
+    height: 25px; /* Ajusta la altura automáticamente */
+    margin-left: 630px;
+    font-size: 56px;
+    color: black;
+  }
+  </style>
+  
---
+++ src/components/NewNavBar.vue
@@ -0,0 +1,132 @@
+<template>
+    <v-app-bar
+      app
+      color="#FCF7F6"
+      dark
+      :elevation="0"
+      :height="visible ? 64 : 0"
+      :class="{ 'scroll-hide': scrolled }"
+    >
+      <!-- Agregar imagen a la izquierda con estilo -->
+      <img
+        :src="require('../assets/logosvg.svg')"
+        class="logo-image"
+      />
+      
+      <!-- Título en el centro del v-app-bar con estilo de fuente personalizado y ubicación modificada -->
+      <v-toolbar-title class="title">
+        ESCUELA DE NEGOCIOS<br />ESAM COCHABAMBA SUCURSAL
+      </v-toolbar-title>
+      
+      <!-- Agregar botones a la derecha con espacio entre ellos -->
+      <v-btn  size="x-small" class="custom-button" id="button1" color="white">
+        REGISTRARSE
+      </v-btn>
+      <v-btn size="x-small" class="mx-2 custom-button" id="button2" color="white">
+        INICIAR SESION
+      </v-btn>
+    </v-app-bar>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        scrolled: false,
+        visible: true,
+        drawer: false,
+        lastScroll: 0,
+      };
+    },
+    mounted() {
+      window.addEventListener("scroll", this.handleScroll);
+    },
+    beforeDestroy() {
+      window.removeEventListener("scroll", this.handleScroll);
+    },
+    methods: {
+      handleScroll() {
+        const currentScroll = window.scrollY;
+  
+        if (currentScroll > this.lastScroll) {
+          // Hacia abajo
+          this.visible = false;
+        } else {
+          // Hacia arriba
+          this.visible = true;
+        }
+  
+        this.lastScroll = currentScroll;
+        this.scrolled = currentScroll > 0;
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .scroll_hide-enter-active,
+  .scroll_hide-leave-active {
+    transition: opacity 0.5s;
+  }
+  .scroll_hide-enter,
+  .scroll_hide-leave-to {
+    opacity: 0;
+  }
+  
+  /* Cambiar el color de la barra de navegación */
+  .v_app_bar {
+    background-color: #3f51b5; /* Cambia este color según tus preferencias */
+  }
+  
+  /* Alinear el título al centro en dos líneas y modificar la ubicación */
+  .title {
+    text-align: center;
+    white-space: normal; /* Cambia a 'normal' para permitir saltos de línea */
+    overflow: hidden;
+    text-overflow: ellipsis;
+    max-width: 100%; /* Asegurar que el título no desborde el v_app_bar */
+    
+    /* Modificar el tipo de letra y aplicar negritas */
+    font-family: "NOW", sans-serif; /* Cambia el tipo de letra según tus preferencias */
+    font-weight: bold; /* Aplicar negritas */
+    
+    /* Modificar la ubicación en X e Y */
+    position: relative;
+    left: 20px; /* Modifica la ubicación horizontal según tus preferencias */
+    top: 0px; /* Modifica la ubicación vertical según tus preferencias */
+  }
+  
+  /* Estilo de la imagen */
+  .logo-image {
+    width: 150px; /* Cambia el ancho según tus preferencias */
+    height: 150px; /* Ajusta la altura automáticamente */
+    
+    /* Mover la imagen hacia abajo y hacia la izquierda */
+    transform: translateY(10px) translateX(55px); /* Ajusta los valores según tu preferencia */
+  }
+  
+  /* Estilo de los botones */
+  .custom-button {
+    transition: background-color 0.3s ease; /* Agrega una transición de color de fondo */
+    border-radius: 300px; /* Redondea los bordes de los botones */
+    color: #D8D2D2; /* Cambia el color del texto a #D8D2D2 */
+    width: 100px; /* Cambia el ancho según tus preferencias */
+    height: 25px; /* Ajusta la altura automáticamente */
+  }
+  
+  .custom-button:hover {
+    background-color: yellow; /* Cambia el color de fondo a amarillo al pasar el cursor */
+  }
+  
+  /* Color de fondo personalizado para Button 1 */
+  #button1 {
+    background-color: #112533;
+    
+  }
+  
+  /* Color de fondo personalizado para Button 2 */
+  #button2 {
+    background-color: #b2a612;
+  }
+  </style>
+  
