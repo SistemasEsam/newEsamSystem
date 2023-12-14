23462839ea96cfe4171784dead49ca1b385a300f
Alan Jimenez Arandia <aljiar23@gmail.com>
2023-12-14 15:03:38 -0400

    Fix image

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+ import { createRouter, createWebHashHistory } from 'vue-router'
 
 import FormView from '../views/FormCVViews/FormView.vue'
 import MainView from '../views/MainView.vue'
@@ -7,8 +7,6 @@ import LoginView from '../views/LoginView.vue'
 import ProfileView from '../views/ProfileView.vue'
 import ContactView from '../views/ContactView.vue'
 import InstructorsView from '@/views/InstructorFinderViews/InstructorsView.vue'
-import StaffMenuView from '@/views/StaffViews/StaffMenuView.vue'
-import AcademicStaffView from '@/views/StaffViews/AcademicStaffView.vue'
 import BlogSingleView from '@/views/BlogViews/BlogSingleView.vue'
 import BlogListView from '@/views/BlogViews/BlogListView.vue'
 import BlogFormView from '@/views/BlogViews/BlogFormView.vue'
@@ -20,69 +18,7 @@ const routes = [
     name: 'main',
     component: MainView
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    path:'/form-view',
-    name:'form-view',
-    component : FormView
-  },
- {
-  path: '/pdf/:id',
-  name: 'pdf',
-  component:PDFView
-
- },
-
-{ 
-  path: '/contact',
-  name: 'contact',
-  component: ContactView
-
-},
-
-
- {
-    path: '/login',
-    name: 'login',
-    component : LoginView
- },
-{
-
-
-  path:'/profile',
-  name:'profile',
-  component:ProfileView
-},
-{
-  path:'/instructorFinder',
-  name: 'instructorFinder',
-  component: InstructorsView
-},
-{
-  path:'/blogSingleView',
-  name: 'blogSingleView',
-  component: BlogSingleView
-},
-{
-  path:'/blogListView',
-  name: 'blogListView',
-  component: BlogListView
-},
-{
-  path:'/blogFormView',
-  name: 'blogFormView',
-  component: BlogFormView
-},
-
 ]
-
 const router = createRouter({
   history: createWebHashHistory(),
   routes
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,37 +1,33 @@
 <template>
-   <NewNavBar/>
-   <LitleNavBar/>
-   <NewImages/>
-   <Programs/>
- 
-  </template>
+  <NewNavBar />
+  <LitleNavBar />
+  <NewImages />
+  <Programs />
+</template>
   
   <script>
-  import NavBar from '@/components/NavBar.vue';
-  import Footer from '@/components/Footer.vue';
-  import Home from '@/components/Home.vue';
-  import NewNavBar from '@/components/NewNavBar.vue'
-  import LitleNavBar from '@/components/LitleNavBar.vue';
-  import NewImages from '@/components/NewImages.vue';
+import NavBar from "@/components/NavBar.vue";
+import Footer from "@/components/Footer.vue";
+import Home from "@/components/Home.vue";
+import NewNavBar from "@/components/NewNavBar.vue";
+import LitleNavBar from "@/components/LitleNavBar.vue";
+import NewImages from "@/components/NewImages.vue";
 
- 
-  export default {
-    name: 'MainView',
-    components: {
-      NavBar,
-      Footer,
-      Home,
-      NewNavBar,
-      LitleNavBar,
-      NewImages,
-     
-      
-    },
-    // Otras opciones y métodos de la vista
-  };
-  </script>
+export default {
+  name: "MainView",
+  components: {
+    NavBar,
+    Footer,
+    Home,
+    NewNavBar,
+    LitleNavBar,
+    NewImages,
+  },
+  // Otras opciones y métodos de la vista
+};
+</script>
   
-  <style scoped>
-  /* Estilos personalizados para la vista */
-  </style>
+  <style>
+/* Estilos personalizados para la vista */
+</style>
   
