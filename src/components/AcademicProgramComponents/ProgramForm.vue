4d986601de1df6a926f42d71f155f9fc9930f0b7
Alan Jimenez Arandia <aljiar23@gmail.com>
2023-12-23 12:04:22 -0400

    Modeling form for programs

--- src/components/AcademicProgramComponents/ProgramForm.vue
+++ src/components/AcademicProgramComponents/ProgramForm.vue
@@ -2,17 +2,31 @@
   <v-form>
     <v-container>
       <v-col>
-        <v-row> 
+        <v-row>
+          <h3>Programa</h3>
+          <v-container>
             <v-text-field label="Nombre del Programa"></v-text-field>
+          </v-container>
         </v-row>
-        <v-row> 
+        <v-row>
+          <h3>Codigo</h3>
+          <v-container>
             <v-text-field label="Código del Programa"></v-text-field>
+          </v-container>
         </v-row>
-        <v-row> 
+        <v-row>
+          <h3>Coordinadora</h3>
+          <v-container>
             <v-text-field label="Coordinadora del Programa"></v-text-field>
+          </v-container>
         </v-row>
-        <v-row> 
+        <v-row>
+          <h3>Modulos</h3>
+          <br>
+          <v-combobox label="Tipo de Programa" :items="['Curso', 'Diplomado', 'Maestria', 'Especialidad']"></v-combobox>
+          <v-container>
             <v-text-field label="Nombre del Modulo"></v-text-field>
+          </v-container>
         </v-row>
       </v-col>
     </v-container>
