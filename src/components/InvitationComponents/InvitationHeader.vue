bfe22001834f0ff80f0a3316db3f817d2fe47fe2
Alan Jimenez Arandia <aljiar23@gmail.com>
2024-02-16 17:57:35 -0400

    Fix header component

--- src/components/InvitationComponents/InvitationHeader.vue
+++ src/components/InvitationComponents/InvitationHeader.vue
@@ -1,61 +1,55 @@
 <template>
-    <div class="header-container">
-      <img :src="logoUrl" alt="Logo" class="logo">
-      <div class="header-text">
-        <h1>UNIVERSIDAD NACIONAL SIGLO XX</h1>
+  <div class="header-container">
+    <div class="header-text">
+        <img :src="logoUrl" alt="Logo" class="logo" />
+      <div class="title-header">
+        <h3>UNIVERSIDAD NACIONAL SIGLO XX</h3>
         <p>Escuela de Postgrado</p>
         <p>Cochabamba</p>
-        <div class="line"></div> <!-- Línea agregada aquí -->
       </div>
     </div>
-  </template>
+    <div class="line"></div>
+    <!-- Línea agregada aquí -->
+  </div>
+</template>
   
   <script>
-  export default {
-    name: 'LetterHeaderComponent',
-    computed: {
-      logoUrl() {
-        return require('@/assets/escudo.svg');
-      }
-    }
-  };
-  </script>
+export default {
+  name: "LetterHeaderComponent",
+  computed: {
+    logoUrl() {
+      return require("@/assets/escudo.svg");
+    },
+  },
+};
+</script>
   
-  <style scoped>
-  .header-container {
-    display: flex;
-    align-items: center;
-    padding: 10px;
-  }
-  
-  .logo {
-    height: 140px;
-    margin-right: 219px;
-    transform: translateX(220px);
-    margin-top: 0px;
-  }
-  
- .header-text h1 {
-  margin: 0;
-  font-weight: bold;
-  font-size: 18px; /* Ajusta el tamaño de la fuente para el título */
+  <style>
+.header-container {
+  display: grid;
+  align-items: center;
+  float: left;
 }
 
-.header-text p {
-  margin: 0;
-  font-size: 16px; /* Ajusta el tamaño de la fuente para los párrafos */
+.logo {
+  height: 140px;
 }
-  
+
+.header-text{
+  font-weight: bold;
+  display:inline-flex;
+}
+.image
+{
+}
+.title-header{
+}
+
 .line {
   height: 2px; /* Controla el grosor de la línea */
-  width: 250%; /* Controla el ancho de la línea */
+  width: 8.5in; /* Controla el ancho de la línea */
   background-color: navy; /* Color azul marino */
-  margin-top: 10px; /* Espaciado vertical desde el texto hasta la línea */
-  margin-left: 50px; /* Mueve la línea horizontalmente a la derecha */
-  margin-right: 50px; /* Ajusta el margen derecho si es necesario */
   position: relative;
-  top: 36px; /* Mueve la línea verticalmente hacia abajo */
-  left: -100px; /* Mueve la línea horizontalmente hacia la derecha */
 }
-  </style>
+</style>
   
--- src/views/InvitationViews/InvitationView.vue
+++ src/views/InvitationViews/InvitationView.vue
@@ -1,38 +1,42 @@
 <template>
+  <div class="main-view">
     <keep-alive>
-      <component :is="activeComponent" @show-next-form="showNextForm" :id-array="idUser" />
+      <component
+        :is="activeComponent"
+        @show-next-form="showNextForm"
+        :id-array="idUser"
+      />
     </keep-alive>
-    
+  </div>
 </template>
 <script>
-import InvitationForm from '@/components/InvitationComponents/InvitationForm.vue'
-import InvitationModuleSection from '@/components/InvitationComponents/InvitationModuleSection.vue';
-import InvitationLetter from '@/components/InvitationComponents/InvitationLetter.vue';
+import InvitationForm from "@/components/InvitationComponents/InvitationForm.vue";
+import InvitationModuleSection from "@/components/InvitationComponents/InvitationModuleSection.vue";
+import InvitationLetter from "@/components/InvitationComponents/InvitationLetter.vue";
 
 export default {
-    setup() {
-        
+  setup() {},
+  components: {
+    InvitationForm,
+    InvitationModuleSection,
+    InvitationLetter,
+  },
+  data() {
+    return {
+      idUser: this.$route.params.id,
+      activeComponent: "invitation-form",
+    };
+  },
+  methods: {
+    showNextForm(nextComponent, idUser) {
+      this.activeComponent = nextComponent;
+      this.idUser = idUser;
     },
-    components:{
-        InvitationForm,
-        InvitationModuleSection,
-        InvitationLetter
-    },
-    data() {
-        return {
-            idUser: this.$route.params.id,
-            activeComponent: 'invitation-form',
-        }
-    },
-    methods: {
-        showNextForm(nextComponent,idUser){
-          this.activeComponent = nextComponent
-          this.idUser =  idUser
-          
-        },
-    }
-}
+  },
+};
 </script>
 <style>
-
+.main-view{
+    position: absolute;
+}
 </style>
